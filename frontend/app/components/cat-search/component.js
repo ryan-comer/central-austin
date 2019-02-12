ae9583949799a55b986625a74f9bb67c7920bd1a
Ryan Comer <ryancomer94@gmail.com>
2019-02-12 01:24:27 -0600

    Fixed for data down action up.

--- frontend/app/components/cat-search/component.js
+++ frontend/app/components/cat-search/component.js
@@ -1,14 +1,5 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-    init() {
-        this._super();
-        this.query = '';
-    },
-
-    actions: {
-        onQueryChange() {
-            alert(this.query);
-        },
-    },
+    
 });
