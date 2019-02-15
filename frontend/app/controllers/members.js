542cab8d6b0a69e1f678413a92a409ec43830750
Ryan Comer <ryancomer94@gmail.com>
2019-02-15 00:39:59 -0600

    Filtering the table is complete.  There is a delay after typing before the list is updated

--- frontend/app/controllers/members.js
+++ frontend/app/controllers/members.js
@@ -1,11 +1,35 @@
 import Controller from '@ember/controller';
+import {run} from '@ember/runloop';
 
 export default Controller.extend({
+    updateWaitTime: 1000,   // Time to wait before updating table
     query: '',
 
     actions: {
         onQueryChange() {
-            alert(this.query);
+            this.set('keyPressed', true);
+            this.set('pressedTime', new Date());
+
+            // Wait until the user stops typing
+            run.later(this, function(){
+                var currentTime = new Date();
+
+                // Check that it's been long enough
+                var elapsedTime = currentTime - this.get('pressedTime');
+                if(elapsedTime >= this.updateWaitTime){
+                    this.filterTable(this.query);
+                }
+            }, this.updateWaitTime);
         },
+    },
+
+    // Filter the table based on the query
+    filterTable(){
+        const query = this.query.trim().toLowerCase() || '';
+        // Update filtered model
+        this.set('filteredModel', this.model.filter(member => {
+            // Filter by name
+            return member.firstName.toLowerCase().includes(query);
+        }));
     },
 });
--- frontend/app/routes/members.js
+++ frontend/app/routes/members.js
@@ -8,4 +8,10 @@ export default Route.extend({
             paths: this.store.findAll('path'),
         });
     },
+
+    setupController(controller, model){
+        this._super(controller, model);
+        // Start with everything
+        this.controller.set('filteredModel', model);
+    },
 });
