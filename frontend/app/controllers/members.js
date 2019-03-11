201a72efd8da5f0418fb46f0cb807eab69c2375f
Ryan Comer <ryancomer94@gmail.com>
2019-03-11 07:40:03 -0500

    Search bar working.  Still need to add tests

--- frontend/app/controllers/members.js
+++ frontend/app/controllers/members.js
@@ -3,25 +3,14 @@ import {run} from '@ember/runloop';
 import {computed} from '@ember/object';
 
 export default Controller.extend({
-    query: '',
-
     // Member list that's actually displayed in the table
     filteredMembers: computed('model.@each.fullName', 'query', function(){
         const query = (this.query || '').trim().toLowerCase();
 
-        return this.model.filter(member => {
+        return this.model.members.filter(member => {
             const fullName = member.fullName.replace(/ /g, '');
 
             return fullName.toLowerCase().includes(query);
         }).sortBy('fullName');
     }),
-
-    init(){
-        this._super(...arguments);
-
-        this.setProperties({
-            query: '',
-        })
-    },
-
 });
