4a763bad1e91a59d644ac644e8a2f3ffb8ba3c7f
ryan-comer <ryancomer94@gmail.com>
2019-03-23 15:00:46 -0500

    Temp commit

--- frontend/tests/acceptance/member-search-filter-test.js
+++ frontend/tests/acceptance/member-search-filter-test.js
@@ -1,7 +1,8 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, fillin, pauseTest } from '@ember/test-helpers';
+import { visit, currentURL, click, typeIn, pauseTest, triggerKeyEvent } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { set } from '@ember/object';
 
 module('Acceptance | member search filter', function(hooks) {
   setupApplicationTest(hooks);
@@ -14,21 +15,51 @@ module('Acceptance | member search filter', function(hooks) {
   });
 
   test('should filter table', async function(assert){
+     // Create the test members
+     server.create('member', {
+      firstName: 'Ryan',
+      lastName: 'Comer',
+      paths: [
+          server.create('path', {
+              name: 'Visionary Communication',
+          }),
+          server.create('path', {
+              name: 'Presentation Mastery',
+          }),
+      ],
+      email: 'r.comer@example.com',
+      phone: '(512) 123-4567', 
+    });
 
-     // Create initial data for the table
-     server.createList('member', 10);
-     /*server.create('member', {firstName: 'Ryan'});
-     server.create('member', {firstName: 'Issac'});
-     server.create('member', {firstName: 'Jessica'});
-     server.create('member', {firstName: 'Fahad'});
-     server.create('member', {firstName: 'Shreya'});*/
+    server.create('member', {
+      firstName: 'Isaac',
+      lastName: 'Lee',
+      paths: [
+          server.create('path', {
+              name: 'Visionary Communication',
+          }),
+          server.create('path', {
+              name: 'Presentation Mastery',
+          }),
+      ],
+      email: 'i.lee@example.com',
+      phone: '(512) 123-4567', 
+      });
 
      await visit('/members');
 
-     //await pauseTest();
+     // Start with 2 members
+     var membersList = this.element.querySelectorAll('[data-test-row]');
+     assert.equal(membersList.length, 2);
 
      var searchBar = this.element.querySelector('[data-test-members-search]');
      assert.ok(searchBar, 'search bar should not be null');
 
+     //await click(find('[data-test-members-search]'));
+     //await triggerKeyEvent(find('[data-test-members-search]'), 'keydown', 'R');
+
+     await pauseTest();
+
+     assert.equal(searchBar.value, "Ryan");
   });
 });
